37a668bcd713d0eccbbaea4e778efa94abd1de35
Nikolas <nik.steppenwolf@web.de>
2025-02-12 16:06:33 +0100

    Selectors added

--- js_dom-events/js-dom-and-events_dark-mode/js/index.js
+++ js_dom-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,6 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const buttonDark = document.querySelector('[data-js="dark-mode-button"]');
+const buttonLight = document.querySelector('[data-js="light-mode-button"]');
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
