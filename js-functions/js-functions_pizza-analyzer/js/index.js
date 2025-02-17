f86d2d3accb3fe9d9b70522f154faf39f45ddb03
Nikolas <nik.steppenwolf@web.de>
2025-02-17 10:40:01 +0100

    solved

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -9,15 +9,39 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // Write your code here
+  updatePizzaDisplay(pizza1, pizzaInput1.value);
+  evalInput();
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // Write your code here
+  updatePizzaDisplay(pizza2, pizzaInput2.value);
+  evalInput();
 });
 
 // Task 1: Define the function `calculatePizzaGain` here
 
+function calculatePizzaGain(diam1, diam2) {
+  const area1 = Math.PI * (diam1 / 2) ** 2;
+  const area2 = Math.PI * (diam2 / 2) ** 2;
+  pizzaGain = Math.round(((area2 - area1) / area1) * 100);
+  return pizzaGain;
+}
+
+function evalInput() {
+  pizzaSize1 = pizzaInput1.value;
+  pizzaSize2 = pizzaInput2.value;
+  const pizzaGainResult = calculatePizzaGain(pizzaSize1, pizzaSize2);
+  output.textContent = pizzaGainResult;
+}
+
 // Task 2: Define the function `updatePizzaDisplay` here
+
+function updatePizzaDisplay(pizzaElement, newSize) {
+  console.log("ElemId -> " + pizzaElement + " -- Val -> " + newSize);
+  const newDisplaySize = (newSize / 24) * 100;
+  const newDisplaySizeValue = newDisplaySize + "px";
+  console.log("resulting in new display size: " + newDisplaySizeValue);
+  pizzaElement.style.width = newDisplaySizeValue;
+}
 
 // Task 3: Define the function `updateOutputColor` here
