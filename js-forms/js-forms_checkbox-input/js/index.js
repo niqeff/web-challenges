38895482c7166b38a26f29bffd9171096b0e5b76
Nikolas <nik.steppenwolf@web.de>
2025-02-18 12:02:34 +0100

    solved

---
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -0,0 +1,43 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const tosError = document.querySelector('[data-js="tos-error"]');
+const tosCheckbox = document.querySelector('[data-js="tos"]');
+
+function hideTosError() {
+  tosError.setAttribute("hidden", "");
+}
+
+function showTosError() {
+  tosError.removeAttribute("hidden");
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // --v-- write your code here --v--
+
+  formElements = new FormData(event.target);
+  formData = Object.fromEntries(formElements);
+
+  console.log(formData);
+
+  const tosChecked = formData.tos;
+  console.log(tosChecked);
+  if (!tosChecked) {
+    return;
+  }
+  // --^-- write your code here --^--
+
+  // eslint-disable-next-line no-alert
+  alert("Form submitted");
+});
+
+form.addEventListener("input", () => {
+  const tosChecked = tosCheckbox.checked;
+  if (tosChecked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
