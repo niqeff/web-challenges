f635bf74cab17902bc2f40650920360cee13b90c
Nikolas <nik.steppenwolf@web.de>
2025-02-18 11:31:14 +0100

    solved

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const inputData = Object.fromEntries(formData);
+
+  const firstName = inputData.firstName;
+  const lastName = inputData.lastName;
+  const age = inputData.age;
+  console.log(firstName, lastName, age);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
