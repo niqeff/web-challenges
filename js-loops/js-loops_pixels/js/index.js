1d084810a9a94269279bd068df816e86fd054efd
Nikolas <nik.steppenwolf@web.de>
2025-02-20 12:57:02 +0100

    solved

--- js-loops/js-loops_pixels/js/index.js
+++ js-loops/js-loops_pixels/js/index.js
@@ -123,3 +123,12 @@ const pixelValues = [
 ];
 
 const canvas = document.querySelector('[data-js="canvas"]');
+
+for (pixelData of pixelValues) {
+  for (colorData of pixelData) {
+    const divElem = document.createElement("div");
+    divElem.classList.add("pixel");
+    divElem.style.backgroundColor("colorData");
+    canvas.append(divElem);
+  }
+}
