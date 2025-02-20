434005f1118acab10f99b0ac8433b5413f799f77
Nikolas <nik.steppenwolf@web.de>
2025-02-20 11:43:21 +0100

    solved

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,12 +2,20 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+  for (let i = 1; i <= 5; i++) {
+    const starElem = document.createElement("img");
+    if (i <= filledStars) {
+      starElem.setAttribute("src", "assets/star-filled.svg");
+    } else {
+      starElem.setAttribute("src", "assets/star-empty.svg");
+    }
+    starContainer.append(starElem);
+  }
   // --^-- write or modify code above this line --^--
 }
 
