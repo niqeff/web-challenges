fdc781a7c56673cf473ea9840e2f3574d6d78ad2
Nikolas <nik.steppenwolf@web.de>
2025-02-24 10:53:35 +0100

    solved

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,13 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const starWarsData = await response.json();
+  console.log(starWarsData);
+  const results = starWarsData.results;
+  const eyeColor = results[2].eye_color;
+  console.log(eyeColor);
+}
 
 fetchData();
