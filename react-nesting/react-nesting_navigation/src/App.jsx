2073b820c71ee41c2e650a3faa7c6487fcfad127
Nikolas <nik.steppenwolf@web.de>
2025-03-03 14:00:37 +0100

    solved

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -1,35 +1,24 @@
 import "./styles.css";
-
+import { Header } from "./components/Header.jsx";
+import { Navigation } from "./components/Navigation.jsx";
+import { Link } from "./components/Link.jsx";
+import { Logo } from "./components/Logo.jsx";
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
+import { Avatar } from "./components/Avatar.jsx";
 
 export default function App() {
   return (
     <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
-        <nav>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
-        </nav>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
-      </header>
-      <main>content goes here…</main>
+      <Header>
+        <Logo imgSrc={logo}></Logo>
+        <Navigation>
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#impressum">Impressum</Link>
+        </Navigation>
+        <Avatar imgSrc={avatar}></Avatar>
+      </Header>
     </>
   );
 }
