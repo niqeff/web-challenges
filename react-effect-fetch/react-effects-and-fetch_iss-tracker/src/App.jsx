09b2e33ae5b89c310692312a70a0455d5a96a2e8
Nikolas <nik.steppenwolf@web.de>
2025-03-05 16:32:13 +0100

    solved

--- react-effect-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
+++ react-effect-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,23 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    const response = await fetch(URL);
+    const currentPosition = await response.json();
+    console.log(currentPosition);
+    setCoords({
+      longitude: currentPosition.longitude,
+      latitude: currentPosition.latitude,
+    });
+  }
+
+  useEffect(() => {
+    const updateISSCoords = setInterval(getISSCoords, 5000);
+    console.log(updateISSCoords);
+    return () => {
+      clearInterval(updateISSCoords);
+    };
+  }, []);
 
   return (
     <main>
